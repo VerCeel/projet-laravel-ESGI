d579e445d19b5828b8bbb988727ff89439383805
VerCeel <hirich62@gmail.com>
2026-04-10 01:53:15 +0200

    add .env.example

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
+const baseApiUrl = import.meta.env.VITE_API_BASE_URL
+  ? `${import.meta.env.VITE_API_BASE_URL.replace(/\/$/, "")}/api`
+  : "/api";
+
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000/api",
+  baseURL: baseApiUrl,
 });
 
 export default api;

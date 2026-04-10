ab31030cc8963fc9769bcdc12bb2c26a9a2b8b7f
VerCeel <hirich62@gmail.com>
2026-04-11 01:12:02 +0200

    fix/laravel CORS

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -8,6 +8,7 @@ return [
 
     'allowed_origins' => [
         'http://localhost:5173', // React dev server
+        'http://165.22.22.160:5173',
     ],
 
     'allowed_origins_patterns' => [],
--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,6 +2,8 @@ import axios from "axios";
 
 const baseApiUrl = `${import.meta.env.VITE_API_BASE_URL}/api`;
 
+console.log(baseApiUrl)
+
 const api = axios.create({
   baseURL: baseApiUrl,
 });

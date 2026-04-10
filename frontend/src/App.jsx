e272bcd5fb49aabc6e4eb01880add918a96de6bd
VerCeel <hirich62@gmail.com>
2026-04-11 01:44:41 +0200

    add tailwincss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from "react";
 import api from "./services/api";
+import NavBar from "./components/static/NavBar";
+import Footer from "./components/static/Footer";
 
 function App() {
   const [message, setMessage] = useState("");
@@ -12,8 +14,10 @@ function App() {
 
   return (
     <div>
+      <NavBar />
       <h1>Test API</h1>
       <p>{message}</p>
+      <Footer />
     </div>
   );
 }
